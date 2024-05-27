b8413e9fcff3559ec4d83b738c1ddb6a6dd6b6e4
Tomas Vazquez <vazquezt2018@gmail.com>
2024-05-26 21:38:23 -0300

    fixed problems with auth

--- turf-frontend/src/Register.jsx
+++ turf-frontend/src/Register.jsx
@@ -1,18 +1,26 @@
 import { useForm } from 'react-hook-form';
 import { useAuth } from './AuthContext';
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 const Register = () => {
     const { register, handleSubmit, formState: { errors } } = useForm();
     const { register: registerUser } = useAuth();
+    const [errorMessage, setErrorMessage] = useState('');
+    const navigate = useNavigate("/home")
 
     const onSubmit = async (data) => {
         try {
-            console.log(data)
             await registerUser(data.email, data.password);
+            navigate("/home")
             // Redirigir o mostrar mensaje de éxito
         } catch (error) {
-            // Manejar error de registro
-            console.error("Error en el registro", error);
+            if (error.response && error.response.status === 400) {
+                setErrorMessage(error.response.data.detail);
+            } else {
+                console.error("Error en el registro", error);
+                setErrorMessage(error.detail);
+            }
         }
     };
 
@@ -34,6 +42,7 @@ const Register = () => {
                 />
                 {errors.password && <span>{errors.password.message}</span>}
             </div>
+            {errorMessage && <div style={{ color: 'red' }}>{errorMessage}</div>}
             <button type="submit">Registrarse</button>
         </form>
     );
--- turf-frontend/src/auth.js
+++ turf-frontend/src/auth.js
@@ -1,26 +1,48 @@
 import axios from 'axios';
 
-const API_URL = 'http://127.0.0.1:8000'; // Asegúrate de que esto apunte a tu servidor FastAPI
+const API_URL = 'http://127.0.0.1:8000';
 
 export const register = async (email, password) => {
-    const response = await axios.post(`${API_URL}/users/`, { email, password });
-    return response.data;
+    try {
+        const response = await axios.post(`${API_URL}/users/`, { email, password });
+        return response.data;
+    } catch (error) {
+        if (error.response) {
+            throw error.response.data;
+        }
+        throw new Error('Ocurrió un error inesperado.');
+    }
 };
 
 export const login = async (email, password) => {
-    const response = await axios.post(`${API_URL}/token/`, `username=${email}&password=${password}`, {
-        headers: {
-            'Content-Type': 'application/x-www-form-urlencoded'
+    try {
+        const response = await axios.post(`${API_URL}/token/`, `username=${email}&password=${password}`, {
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded'
+            }
+        });        
+        return response.data;
+    } catch (error) {
+        console.log(error)
+        if (error.response) {
+            throw error.response.data;
         }
-    });
-    return response.data;
+        throw new Error('Ocurrió un error inesperado.');
+    }
 };
 
 export const getProfile = async (token) => {
-    const response = await axios.get(`${API_URL}/users/me/`, {
-        headers: {
-            Authorization: `Bearer ${token}`,
-        },
-    });
-    return response.data;
+    try {
+        const response = await axios.get(`${API_URL}/users/me/`, {
+            headers: {
+                Authorization: `Bearer ${token}`,
+            },
+        });
+        return response.data;
+    } catch (error) {
+        if (error.response) {
+            throw error.response.data;
+        }
+        throw new Error('Ocurrió un error inesperado.');
+    }
 };
