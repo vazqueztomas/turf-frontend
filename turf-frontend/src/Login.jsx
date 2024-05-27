4d4c52b8c18e397c072bb1e3a0566748f9779531
Tomas Vazquez <vazquezt2018@gmail.com>
2024-05-26 22:24:35 -0300

    added styled in forms

--- turf-frontend/src/Login.jsx
+++ turf-frontend/src/Login.jsx
@@ -1,51 +1,61 @@
-import { useForm } from 'react-hook-form';
-import { useAuth } from './AuthContext';
-import { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
+import { useForm } from "react-hook-form";
+import { useAuth } from "./AuthContext";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { Button, Input, FormLabel, FormControl, Divider } from "@chakra-ui/react";
 
 const Login = () => {
-    const { register, handleSubmit, formState: { errors } } = useForm();
-    const { login } = useAuth();
-    const [errorMessage, setErrorMessage] = useState('');
-    const navigate = useNavigate("/home")
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isSubmitting },
+  } = useForm();
+  const { login } = useAuth();
+  const [errorMessage, setErrorMessage] = useState("");
+  const navigate = useNavigate("/home");
 
-    const onSubmit = async (data) => {
-        try {
-            await login(data.email, data.password);
-            // Redirigir o mostrar mensaje de éxito
-            navigate("/home")
-        } catch (error) {
-            if (error.response && error.response.status === 400) {
-                setErrorMessage(error.response.data.detail);
-            } else {
-                console.error("Error en el registro", error);
-                setErrorMessage(error.detail);
-            }
-        }
-    };
+  const onSubmit = async data => {
+    try {
+      await login(data.email, data.password);
+      // Redirigir o mostrar mensaje de éxito
+      navigate("/home");
+    } catch (error) {
+      if (error.response && error.response.status === 400) {
+        setErrorMessage(error.response.data.detail);
+      } else {
+        console.error("Error en el registro", error);
+        setErrorMessage(error.detail);
+      }
+    }
+  };
 
-    return (
-        <form onSubmit={handleSubmit(onSubmit)}>
-            <div>
-                <label>Email:</label>
-                <input
-                    type="email"
-                    {...register('email', { required: 'Este campo es obligatorio' })}
-                />
-                {errors.email && <span>{errors.email.message}</span>}
-            </div>
-            <div>
-                <label>Contraseña:</label>
-                <input
-                    type="password"
-                    {...register('password', { required: 'Este campo es obligatorio' })}
-                />
-                {errors.password && <span>{errors.password.message}</span>}
-            </div>
-            {errorMessage && <div style={{ color: 'red' }}>{errorMessage}</div>}
-            <button type="submit">Iniciar sesión</button>
-        </form>
-    );
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <FormControl>
+        <div>
+          <FormLabel>Email:</FormLabel>
+          <Input
+            type="email"
+            {...register("email", { required: "Este campo es obligatorio" })}
+          />
+          {errors.email && <span>{errors.email.message}</span>}
+        </div>
+        <div>
+          <FormLabel>Contraseña:</FormLabel>
+          <Input
+            type="password"
+            {...register("password", { required: "Este campo es obligatorio" })}
+          />
+          {errors.password && <span>{errors.password.message}</span>}
+        </div>
+        <Divider my={4}/>
+        {errorMessage && <div style={{ color: "red" }}>{errorMessage}</div>}
+      </FormControl>
+      <Button type="submit" isLoading={isSubmitting}>
+        Iniciar sesión
+      </Button>
+    </form>
+  );
 };
 
 export default Login;
--- turf-frontend/src/Register.jsx
+++ turf-frontend/src/Register.jsx
@@ -1,51 +1,61 @@
-import { useForm } from 'react-hook-form';
-import { useAuth } from './AuthContext';
-import { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
+import { useForm } from "react-hook-form";
+import { useAuth } from "./AuthContext";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { Button, Divider, FormControl, FormLabel, Input } from "@chakra-ui/react";
 
 const Register = () => {
-    const { register, handleSubmit, formState: { errors } } = useForm();
-    const { register: registerUser } = useAuth();
-    const [errorMessage, setErrorMessage] = useState('');
-    const navigate = useNavigate("/home")
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isSubmitting },
+  } = useForm();
+  const { register: registerUser } = useAuth();
+  const [errorMessage, setErrorMessage] = useState("");
+  const navigate = useNavigate("/home");
 
-    const onSubmit = async (data) => {
-        try {
-            await registerUser(data.email, data.password);
-            navigate("/home")
-            // Redirigir o mostrar mensaje de éxito
-        } catch (error) {
-            if (error.response && error.response.status === 400) {
-                setErrorMessage(error.response.data.detail);
-            } else {
-                console.error("Error en el registro", error);
-                setErrorMessage(error.detail);
-            }
-        }
-    };
+  const onSubmit = async data => {
+    try {
+      await registerUser(data.email, data.password);
+      navigate("/home");
+      // Redirigir o mostrar mensaje de éxito
+    } catch (error) {
+      if (error.response && error.response.status === 400) {
+        setErrorMessage(error.response.data.detail);
+      } else {
+        console.error("Error en el registro", error);
+        setErrorMessage(error.detail);
+      }
+    }
+  };
 
-    return (
-        <form onSubmit={handleSubmit(onSubmit)}>
-            <div>
-                <label>Email:</label>
-                <input
-                    type="email"
-                    {...register('email', { required: 'Este campo es obligatorio' })}
-                />
-                {errors.email && <span>{errors.email.message}</span>}
-            </div>
-            <div>
-                <label>Contraseña:</label>
-                <input
-                    type="password"
-                    {...register('password', { required: 'Este campo es obligatorio' })}
-                />
-                {errors.password && <span>{errors.password.message}</span>}
-            </div>
-            {errorMessage && <div style={{ color: 'red' }}>{errorMessage}</div>}
-            <button type="submit">Registrarse</button>
-        </form>
-    );
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <FormControl>
+        <div>
+          <FormLabel>Email:</FormLabel>
+          <Input
+            type="email"
+            {...register("email", { required: "Este campo es obligatorio" })}
+          />
+          {errors.email && <span>{errors.email.message}</span>}
+        </div>
+        <div>
+          <FormLabel>Contraseña:</FormLabel>
+          <Input
+            type="password"
+            {...register("password", { required: "Este campo es obligatorio" })}
+          />
+          {errors.password && <span>{errors.password.message}</span>}
+        </div>
+      </FormControl>
+      <Divider my={4}/>
+      {errorMessage && <div style={{ color: "red" }}>{errorMessage}</div>}
+      <Button type="submit" isLoading={isSubmitting}>
+        Registrarse
+      </Button>
+    </form>
+  );
 };
 
 export default Register;
