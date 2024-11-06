9c5a1a200dcb785c2e64c1c1ee1d325fbc83e889
Tomas Vazquez <vazquezt2018@gmail.com>
2024-11-06 09:52:08 -0300

    fix(config): use auth_context

--- src/Login/Login.tsx
+++ src/Login/Login.tsx
@@ -1,78 +1,81 @@
-import React, { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
-import { loginUser } from '../api/api';
+import React, { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+import { loginUser } from '../api/api'
+import { useAuth } from '../AuthContext'
 
 const Login: React.FC = () => {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [error, setError] = useState<string | null>(null);
-  const [success, setSuccess] = useState<string | null>(null);
-  const navigate = useNavigate();
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    const [error, setError] = useState<string | null>(null)
+    const [success, setSuccess] = useState<string | null>(null)
+    const navigate = useNavigate()
+    const { loginAuthContext } = useAuth()
 
-  const handleLogin = async (e: React.FormEvent) => {
-    e.preventDefault();
+    const handleLogin = async (e: React.FormEvent) => {
+        e.preventDefault()
 
-    try {
-      await loginUser(email, password);
-      setSuccess('Inicio de sesión exitoso');
-      setError(null);
-      navigate('/programs'); // Redirigir a la página de programas después del inicio de sesión exitoso
-    } catch (error) {
-      // eslint-disable-next-line @typescript-eslint/no-explicit-any
-      setError((error as any).message);
-      setSuccess(null);
+        try {
+            await loginUser(email, password)
+            setSuccess('Inicio de sesión exitoso')
+            setError(null)
+            loginAuthContext()
+            navigate('/programs')
+        } catch (error) {
+            // eslint-disable-next-line @typescript-eslint/no-explicit-any
+            setError((error as any).message)
+            setSuccess(null)
+        }
     }
-  };
 
-  return (
-    <div className="min-h-screen flex items-center justify-center bg-gray-100">
-      <div className="bg-white p-8 rounded shadow-md w-full max-w-md">
-        <h2 className="text-2xl font-bold mb-6 text-center">Iniciar Sesión</h2>
-        <form onSubmit={handleLogin}>
-          <div className="mb-4">
-            <label className="block text-gray-700 mb-2" htmlFor="email">
-              Correo Electrónico
-            </label>
-            <input
-              type="email"
-              id="email"
-              className="w-full px-3 py-2 border rounded"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-              required
-            />
-          </div>
-          <div className="mb-6">
-            <label className="block text-gray-700 mb-2" htmlFor="password">
-              Contraseña
-            </label>
-            <input
-              type="password"
-              id="password"
-              className="w-full px-3 py-2 border rounded"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-              required
-            />
-          </div>
-          <button
-            type="submit"
-            className="w-full bg-blue-500 text-white py-2 rounded hover:bg-blue-700 mb-4"
-          >
-            Iniciar Sesión
-          </button>
-        </form>
-        <button
-          className="w-full bg-gray-500 text-white py-2 rounded hover:bg-gray-700"
-          onClick={() => navigate('/register')}
-        >
-          Registrarse
-        </button>
-        {error && <p className="mt-4 text-red-500">{error}</p>}
-        {success && <p className="mt-4 text-green-500">{success}</p>}
-      </div>
-    </div>
-  );
-};
+    return (
+        <div className="min-h-screen flex items-center justify-center bg-gray-100">
+            <div className="bg-white p-8 rounded shadow-md w-full max-w-md">
+                <h2 className="text-2xl font-bold mb-6 text-center">Iniciar Sesión</h2>
+                <form onSubmit={handleLogin}>
+                    <div className="mb-4">
+                        <label className="block text-gray-700 mb-2" htmlFor="email">
+                            Correo Electrónico
+                        </label>
+                        <input
+                            type="email"
+                            id="email"
+                            className="w-full px-3 py-2 border rounded"
+                            value={email}
+                            onChange={e => setEmail(e.target.value)}
+                            required
+                        />
+                    </div>
+                    <div className="mb-6">
+                        <label className="block text-gray-700 mb-2" htmlFor="password">
+                            Contraseña
+                        </label>
+                        <input
+                            type="password"
+                            id="password"
+                            className="w-full px-3 py-2 border rounded"
+                            value={password}
+                            onChange={e => setPassword(e.target.value)}
+                            required
+                        />
+                    </div>
+                    <button
+                        type="submit"
+                        className="w-full bg-blue-500 text-white py-2 rounded hover:bg-blue-700 mb-4"
+                    >
+                        Iniciar Sesión
+                    </button>
+                </form>
+                <button
+                    className="w-full bg-gray-500 text-white py-2 rounded hover:bg-gray-700"
+                    onClick={() => navigate('/register')}
+                >
+                    Registrarse
+                </button>
+                {error && <p className="mt-4 text-red-500">{error}</p>}
+                {success && <p className="mt-4 text-green-500">{success}</p>}
+            </div>
+        </div>
+    )
+}
 
-export default Login;
+export default Login
