a6e73b26c9f085b7da675daea27e992363b70f2f
Tomas Vazquez <vazquezt2018@gmail.com>
2024-11-08 16:44:04 -0300

    fix(config) add new navbar

---
+++ src/assets/NavbarButton.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+
+interface NavbarButtonProps {
+  label: string;
+  path: string;
+}
+
+const NavbarButton: React.FC<NavbarButtonProps> = ({ label, path }) => {
+  const navigate = useNavigate();
+
+  return (
+    <button
+      className="px-2 py-2 bg-blue-700 rounded hover:bg-blue-900 text-xs"
+      onClick={() => navigate(path)}
+    >
+      {label}
+    </button>
+  );
+};
+
+export default NavbarButton;
