b884f08042faf493ccee751c36a49194bcde8448
Tomas Vazquez <vazquezt2018@gmail.com>
2024-11-28 13:08:27 -0300

    feat(config): add PDF viewer component and routes

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,14 @@ import Programs from './Programs'
 import ProgramDetail from './ProgramDetail'
 import Login from './Login/Login'
 import Register from './Register/Register'
+import PdfComponent from './PdfViewer/PdfViewer'
 
 export default function App() {
     return (
         <Router>
             <Routes>
                 <Route path="/" element={<Home />} />
+                <Route path="/pdf" element={<PdfComponent />} />
                 <Route path="/login" element={<Login />} />
                 <Route path="/register" element={<Register />} />
                 <Route path="/programs" element={<Programs />} />
---
+++ src/PdfViewer/PdfViewer.tsx
@@ -0,0 +1,30 @@
+import { Worker, Viewer } from '@react-pdf-viewer/core'
+import '@react-pdf-viewer/core/lib/styles/index.css'
+import '@react-pdf-viewer/default-layout/lib/styles/index.css'
+import { defaultLayoutPlugin } from '@react-pdf-viewer/default-layout'
+import pdf_palermo from '../assets/example.pdf'
+import './PdfViewer.css' // Import the CSS file
+
+const Pdf: React.FC = () => {
+    const defaultLayoutPluginInstance = defaultLayoutPlugin()
+    return (
+        <div className="pdf-container">
+            <Worker workerUrl={`https://unpkg.com/pdfjs-dist@3.11.174/build/pdf.worker.min.js`}>
+                <Viewer fileUrl={pdf_palermo} plugins={[defaultLayoutPluginInstance]} />
+            </Worker>
+        </div>
+    )
+}
+
+const PdfComponent: React.FC = () => {
+    return (
+        <div>
+            <div style={{ width: '50%', margin: '0 auto' }}>
+                <Pdf />
+            </div>
+            <Pdf />
+        </div>
+    )
+}
+
+export default PdfComponent
